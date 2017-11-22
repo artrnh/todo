4f344e2c98cec7edfe50b732961dfb91f420dd6d
artrnh <artrnh@gmail.com>
2017-11-23 02:28:17 +0300

    svg hotfix

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -20,20 +20,24 @@ export default class Task extends Component {
   }
 
   render() {
-    const doneBtnStyles = this.props.item.done  ? { borderColor: '#74AF96' } : {};
-    const doneImgStyles = this.props.item.done  ? { display: 'block' } : {};
+    const doneBtnStyles = this.props.item.done ? { borderColor: '#74AF96' } : {};
+    const doneImgStyles = this.props.item.done ? { display: 'block' } : {};
     const doneTaskStyles = this.props.item.done ? { textDecoration: 'line-through', color: '#747E80' } : {};
     return (
       <tr className="row">
         <td className="checkTd">
           <button style={doneBtnStyles} onClick={this.handleDoneClick}>
-            <img style={doneImgStyles} src='https:icon.now.sh/check/32/74AF96' alt='check icon' />
+            <svg style={doneImgStyles} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="#74AF96">
+              <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z "></path>
+            </svg>
           </button>
         </td>
         <td style={doneTaskStyles} className="task">{this.props.item.text}</td>
         <td className="deleteTd">
           <button onClick={this.handleDeleteClick}>
-            <img src='https:icon.now.sh/close/32/CF4037' alt='close icon' />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="#CF4037">
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z "></path>
+            </svg>
           </button>
         </td>
       </tr>
