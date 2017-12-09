d296bc2aa4413d9479acb1a385985076b43d88e7
artrnh <artrnh@gmail.com>
2017-12-09 16:50:21 +0300

    sw deleted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TodoApp from './containers/TodoApp/TodoApp';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<TodoApp />, document.getElementById('root'));
-registerServiceWorker();
